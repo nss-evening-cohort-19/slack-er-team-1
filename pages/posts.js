e7db9f2f80b54b47abf6be8e8fe0b21f1fe7ee09
Kristen Chandler <krismchandler@gmail.com>
2022-08-04 21:07:03 -0500

    props

--- pages/posts.js
+++ pages/posts.js
@@ -1,35 +1,35 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
-import PostCard from '../components/PostCard';
-import Search from '../components/Search';
+// /* eslint-disable react-hooks/exhaustive-deps */
+// import React, { useEffect, useState } from 'react';
+// import PostCard from '../components/PostCard';
+// import Search from '../components/Search';
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [filteredPosts, setFilteredPosts] = useState([]);
+// export default function Posts() {
+//   const [posts, setPosts] = useState([]);
+//   const [filteredPosts, setFilteredPosts] = useState([]);
 
-  const getAllPosts = () => {
-    getAllPosts().then((post) => {
-      setPosts(post);
-      setFilteredPosts(post);
-    });
-  };
+//   const getAllPosts = () => {
+//     getPosts().then((post) => {
+//       setPosts(post);
+//       setFilteredPosts(post);
+//     });
+//   };
 
-  useEffect(() => {
-    getAllPosts();
-  }, []);
+//   useEffect(() => {
+//     getAllPosts();
+//   }, []);
 
-  return (
-    <>
-      <div className="text-center my-4 teamCardsDiv">
-        <div className="postsHeaderDiv">
-          <Search posts={posts} setFilteredPosts={setFilteredPosts} />
-        </div>
-        <div className="d-flex flex-wrap postsCardContainer">
-          {filteredPosts.map((post) => (
-            <PostCard key={post.firebaseKey} postObj={posts} onUpdate={getAllPosts} />
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className="text-center my-4 teamCardsDiv">
+//         <div className="postsHeaderDiv">
+//           <Search posts={posts} setFilteredPosts={setFilteredPosts} />
+//         </div>
+//         <div className="d-flex flex-wrap postsCardContainer">
+//           {filteredPosts.map((post) => (
+//             <PostCard key={post.firebaseKey} postObj={posts} onUpdate={getAllPosts} />
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
