04a34cfd31d5e425a8bcd3d686e48db37404afb0
Kristen Chandler <krismchandler@gmail.com>
2022-08-04 20:39:04 -0500

    found one part I missed and updated it to posts

--- pages/posts.js
+++ pages/posts.js
@@ -21,10 +21,10 @@ export default function Posts() {
   return (
     <>
       <div className="text-center my-4 teamCardsDiv">
-        <div className="threadHeaderDiv">
+        <div className="postsHeaderDiv">
           <Search posts={posts} setFilteredPosts={setFilteredPosts} />
         </div>
-        <div className="d-flex flex-wrap messagesCardContainer">
+        <div className="d-flex flex-wrap postsCardContainer">
           {filteredPosts.map((post) => (
             <PostCard key={post.firebaseKey} postObj={posts} onUpdate={getAllPosts} />
           ))}
