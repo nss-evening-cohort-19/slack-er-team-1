85044659564ee64942c035a2cdb350f3ba0bba49
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-06 13:35:28 -0500

    Created a new channel form for users. Also changed the h1 on the form page to a white color to see it.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -29,7 +29,7 @@ export default function Sidebar() {
           </button>
           <ul className="dropdown-menu">
             {channelList.map((channel) => <li key={channel.firebaseKey}><Link className="dropdown-item" href={`/channel/${channel.firebaseKey}`}>{channel.channelName}</Link></li>)}
-            <li><Link className="dropdown-item" href="/">+ Add channel</Link></li>
+            <li><Link className="dropdown-item" href="/channel/new">+ Add channel</Link></li>
           </ul>
         </div>
         <br />
