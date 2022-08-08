074a2cf1c18eee3a9beab1748e304e00b6139c36
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-08-08 11:01:46 -0500

    Pushed up for bug

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import Link from 'next/link';
 // import Search from './Search';
-// import Image from 'next/image';
+import Image from 'next/image';
 import { signOut } from '../utils/auth';
-// import { useAuth } from '../utils/context/authContext';
+import { useAuth } from '../utils/context/authContext';
 import ProfileDropdown from './ProfileDropdown';
 
 export default function NavBar() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
+  console.warn(user.photoURL);
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container-fluid">
@@ -32,7 +33,7 @@ export default function NavBar() {
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                {/* <Image src={user.photoURL} width="30px" height="30px" alt="user" className="user-icon" /> */}
+                <Image src={user.photoURL} width="30px" height="30px" alt="user" className="user-icon" />
               </a>
               <ul className="dropdown-menu">
                 <div>
