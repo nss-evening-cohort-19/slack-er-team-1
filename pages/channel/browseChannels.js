9373a5cc04dcfe87c06b5f9fa800979a2d0d1b55
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-08 19:24:39 -0500

    Added an h1 header to browseChannels.js to make sure the routing is working

---
+++ pages/channel/browseChannels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function browseChannels() {
+  return (
+    <div>
+      <header>
+        <h1>Browse Channels</h1>
+      </header>
+    </div>
+  );
+}
