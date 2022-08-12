a414ce1e5a9ce1825c63b3a9b341bfcc875ed8cf
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-11 22:15:02 -0500

    Added ternaries to return statement for ChannelNameCard to show a different color and text of the btn depending on if the user is already a part of the channel or not

--- components/channels/ChannelNameCard.js
+++ components/channels/ChannelNameCard.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import { useAuth } from '../../utils/context/authContext';
 import { getChannels } from '../../api/channelData';
 
 function ChannelNameCard() {
   const [allChannels, setAllChannels] = useState([]);
+  const { user } = useAuth();
 
   useEffect(() => {
     getChannels().then(setAllChannels);
@@ -12,7 +13,7 @@ function ChannelNameCard() {
   return (
     <div>
       <ul>
-        {allChannels?.map((channel) => <li key={channel.firebaseKey}>{channel.channelName} <button type="button" className="btn btn-outline-success">Join</button></li>)}
+        {allChannels?.map((channel) => <li key={channel.firebaseKey}>{channel.channelName} <button type="button" className={user.uid === channel.uid ? 'btn btn-outline-dark' : 'btn btn-outline-success'}>{user.uid === channel.uid ? 'Leave' : 'Join'}</button></li>)}
       </ul>
     </div>
   );
