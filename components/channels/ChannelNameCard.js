c58492d8234baa3f50ba806b9c269650c8b1b458
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-11 22:22:39 -0500

    Created functions for the onClick of the btn on page. One function is to create the join table with the channel firebaseKey and the uid. The other is to delete the join table if a user wants to leave the channel

--- components/channels/ChannelNameCard.js
+++ components/channels/ChannelNameCard.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../utils/context/authContext';
 import { getChannels } from '../../api/channelData';
+import { createJoinChannel } from '../../api/joinData';
 
 function ChannelNameCard() {
   const [allChannels, setAllChannels] = useState([]);
   const { user } = useAuth();
 
+  const deleteTheJoinObj = () => {
+    console.warn('Future logic to delete join table');
+  };
+
   useEffect(() => {
     getChannels().then(setAllChannels);
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      // channelId: ,
+      userId: user.uid,
+    };
+    createJoinChannel(payload);
+  };
+
   return (
     <div>
       <ul>
-        {allChannels?.map((channel) => <li key={channel.firebaseKey}>{channel.channelName} <button type="button" className={user.uid === channel.uid ? 'btn btn-outline-dark' : 'btn btn-outline-success'}>{user.uid === channel.uid ? 'Leave' : 'Join'}</button></li>)}
+        {allChannels?.map((channel) => <li key={channel.firebaseKey}>{channel.channelName} <button type="button" className={user.uid === channel.uid ? 'btn btn-outline-dark' : 'btn btn-outline-success'} onClick={user.uid === channel.uid ? deleteTheJoinObj : handleSubmit}>{user.uid === channel.uid ? 'Leave' : 'Join'}</button></li>)}
       </ul>
     </div>
   );
